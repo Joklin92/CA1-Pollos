c9b8a9cc4834a32922f28b8f84f3bb2b92140a54
Dennis <joklin@live.dk>
2018-09-05 12:39:20 +0200

    typing speed tester, page 3 - DONE

---
+++ src/main/webapp/page3.js
@@ -0,0 +1,200 @@
+var boys = ["Peter", "lars", "Ole"];
+var girls = ["Janne","hanne","Sanne"];
+
+var all = boys.concat(girls);
+console.log(all);
+//Displaying in HTML side
+document.getElementById("boys").innerHTML = boys.join(', ');
+document.getElementById("girls").innerHTML = girls.join(', ');
+document.getElementById("all").innerHTML = all.join(', ');
+var clickCounter = 0;
+
+/*
+make it so when you are adding a boy/girk, it adds to the boy/girl list, and all list
+*/
+var boyAddTag = document.getElementById("addboy").onclick = addBoy;
+function addBoy(){
+    const boy = document.getElementById('newboy').value;
+    console.log(boys);
+    console.log(all);
+
+    console.log(boy);
+    console.log("Adding boy after break point here!");
+    
+    boys.push(boy);
+    all.push(boy);
+    console.log(boys);
+    console.log(all);
+
+    document.getElementById("boys").innerHTML = boys.join(', ');
+    document.getElementById("all").innerHTML = all.join(', ');
+};
+
+var girlAddTag = document.getElementById("addgirl").onclick = addGirl;
+function addGirl(){
+    const girl = document.getElementById('newgirl').value;
+    console.log(girls);
+    console.log(all);
+
+    console.log(girl);
+    console.log("Adding girl after break point here!");
+
+    girls.push(girl);
+    all.push(girl);
+    console.log(girls);
+    console.log(all);
+
+    document.getElementById("girls").innerHTML = girls.join(', ');
+    document.getElementById("all").innerHTML = all.join(', ');
+};
+
+/*
+Makes it so when you remove a person it removes the specific one
+*/
+var boyRemoveTag = document.getElementById("removeboy").onclick = removeBoy;
+
+function removeBoy(){
+    const toRemove = document.getElementById('newboy').value;    
+    console.log(boys)
+    console.log(all);
+
+    console.log(toRemove);
+    console.log("Removing boy after this break point!");
+
+    var foundAll = all.indexOf(toRemove);
+    var foundBoy = boys.indexOf(toRemove);
+
+    while (foundAll !== -1 && foundBoy !== -1) {
+        all.splice(foundAll, 1);
+        foundAll = all.indexOf(toRemove);
+        boys.splice(foundBoy, 1);
+        foundBoy = boys.indexOf(toRemove);
+
+        console.log(boys);
+        console.log(all);
+
+        document.getElementById("boys").innerHTML = boys.join(', ');
+        document.getElementById("all").innerHTML = all.join(', ');
+    }
+};
+
+var girlRemoveTag = document.getElementById("removegirl").onclick = removeGirl;
+
+function removeGirl(){
+    const toRemove = document.getElementById('newgirl').value;    
+    console.log(girls)
+    console.log(all);
+
+    console.log(toRemove);
+    console.log("Removing girl after this break point!");
+
+    var foundAll = all.indexOf(toRemove);
+    var foundGirl = girls.indexOf(toRemove);
+
+    while (foundAll !== -1 && foundGirl !== -1) {
+        all.splice(foundAll, 1);
+        foundAll = all.indexOf(toRemove);
+        girls.splice(foundGirl, 1);
+        foundGirl = girls.indexOf(toRemove);
+
+        console.log(girls);
+        console.log(all);
+
+        document.getElementById("girls").innerHTML = girls.join(', ');
+        document.getElementById("all").innerHTML = all.join(', ');
+    }
+};
+
+/*
+Reverses the order
+*/
+var reverseAll = document.getElementById("reverse").onclick = reverseOrder;
+function reverseOrder(){
+    all.reverse();
+    console.log(all.join(', '));
+    document.getElementById("all").innerHTML = all.join(', ');
+};
+
+document.getElementById("sort").onclick = function(e){
+    clickCounter += 1;
+    all.sort(sortingOrder);
+    console.log("click: " + clickCounter);
+    if(clickCounter % 2 === 0)
+    {
+        all.reverse(reverseOrder);
+        console.log("Names reversed: ", all); 
+         
+    } else {
+        console.log("Names Sorted: " + all);
+    }
+    document.getElementById("all").innerHTML = all.join(', ');
+}
+
+function sortingOrder(a, b) {
+    /* Storing case insensitive comparison */
+    var comparison = a.toLowerCase().localeCompare(b.toLowerCase());
+    /* If strings are equal in case insensitive comparison */
+    if (comparison === 0) {
+        /* Return case sensitive comparison instead */
+        return a.localeCompare(b);
+    }
+    /* Otherwise return result */
+    
+    return comparison;
+};
+
+document.getElementById("first").onclick = removeFirst;
+function removeFirst(){
+    all.shift(all);
+    console.log(all);
+};
+
+document.getElementById("last").onclick = removeLast;
+function removeLast(){
+    all.pop(all);
+    console.log(all);
+};
+
+//Removes first element from EVERY list after they are sorted
+document.getElementById("first").onclick = function(e){
+    var sortedNames = all.sort(sortingOrder);
+    var sortedBoys = boys.sort(sortingOrder);
+    var sortedGirls = girls.sort(sortingOrder);
+    console.log(sortedNames);
+    console.log(sortedBoys);
+    console.log(sortedGirls);
+
+    console.log("Removing first element after this");
+    all.shift();
+    boys.shift();
+    girls.shift();
+
+    console.log(all.join(', '));
+    console.log(boys.join(', '));
+    console.log(girls.join(', '));
+    document.getElementById("all").innerHTML = all.join(', ');
+    document.getElementById("boys").innerHTML = boys.join(', ');
+    document.getElementById("girls").innerHTML = girls.join(', ');
+};
+
+document.getElementById("last").onclick = function(e){
+    var sortedNames = all.sort(sortingOrder);
+    var sortedBoys = boys.sort(sortingOrder);
+    var sortedGirls = girls.sort(sortingOrder);
+    
+    console.log(sortedNames);
+    console.log(sortedBoys);
+    console.log(sortedGirls);
+
+    console.log("Removing first element after this");
+    all.pop();
+    boys.pop();
+    girls.pop();
+
+    console.log(all.join(', '));
+    console.log(boys.join(', '));
+    console.log(girls.join(', '));
+    document.getElementById("all").innerHTML = all.join(', ');
+    document.getElementById("boys").innerHTML = boys.join(', ');
+    document.getElementById("girls").innerHTML = girls.join(', ');
+}
---
+++ src/main/webapp/speedTest.js
@@ -0,0 +1,90 @@
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
+
+
+var timer = [0,0,0,0];
+var interval;
+var timerRunning = false;
+var testText = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+ "Times New Roman is a serif typeface designed for legibility in body text. It was commissioned by the British newspaper The Times in 1931 and conceived by Stanley Morison, the artistic advisor to the British branch of the printing equipment company Monotype, in collaboration with Victor Lardent, a lettering artist in the Times' advertising department.",
+ "A sports car, or sportscar, is a small, usually two-seater automobile designed for spirited performance and nimble handling. The term sports car was used in The Times, London in 1919.",
+ "Kumquats are a group of small fruit-bearing trees in the flowering plant family Rutaceae.",
+ "Microsoft Windows is a group of several graphical operating system families, all of which are developed, marketed, and sold by Microsoft.",
+ "Java is a general-purpose computer-programming language that is concurrent, class-based, object-oriented, and specifically designed to have as few implementation dependencies as possible. It is intended to let application developers write once, run anywhere , meaning that compiled Java code can run on all platforms that support Java without the need for recompilation.",
+ "Avada Kedavra",
+ "A computer mouse is a hand-held pointing device that detects two-dimensional motion relative to a surface. This motion is typically translated into the motion of a pointer on a display, which allows a smooth control of the graphical user interface. The first public demonstration of a mouse controlling a computer system was in 1968.",
+ "Professor Albus Percival Wulfric Brian Dumbledore is the headmaster of the wizarding school Hogwarts. He is the founder and leader of the Order of the Phoenix, an organisation dedicated to fighting Lord Voldemort.",
+ "Gimli, the dwarf, is chosen to represent the race of Dwarves in the Fellowship of the Ring. As such, he is one of the primary characters of the novel. In the course of the adventure, Gimli aids the Ring-bearer Frodo Baggins, participates in the War of the Ring, and becomes close friends with Legolas, overcoming an ancient enmity of Dwarves and Elves."];
+
+function randomizer(testTexts) {	
+	return testText[Math.floor(Math.random()*Math.floor(testTexts))];	
+}
+
+var sss = document.getElementById("origin-text").innerText = randomizer(testText.length);
+
+function leadingZero(time) {
+    if (time <= 9) {
+        time = "0" + time;
+    }
+    return time;
+}
+
+// Run a standard minute/second/hundredths timer:
+function runTimer() {
+	let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+	theTimer.innerHTML = currentTime;
+	timer[3]++;
+
+	timer[0] = Math.floor((timer[3]/100)/60);
+	timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+	timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
+
+// Match the text entered with the provided text on the page:
+function spellCheck() {
+	let textEntered = testArea.value;
+	let originTextMatch = originText.substring(0,textEntered.length);
+
+	if (textEntered == sss) {
+		clearInterval(interval);
+		testWrapper.style.borderColor = "#429890";
+	} else {
+		if (textEntered == originTextMatch) {
+			testWrapper.style.borderColor = "#65CCf3";
+		} else {
+			testWrapper.style.borderColor = "#E95D0F";
+		}
+	}
+}
+
+// Start the timer:
+function start() {
+	let textEnterdLength = testArea.value.length;
+	if (textEnterdLength === 0 && !timerRunning) {
+		timerRunning = true;
+		interval = setInterval(runTimer, 10);
+	}
+
+}
+
+// Reset everything:
+function reset() {
+	clearInterval(interval);
+	interval = null;
+	timer = [0,0,0,0];
+	timerRunning = false;
+	document.getElementById("origin-text").innerText = randomizer(testText.length);
+	testArea.value = "";
+	theTimer.innerHTML = "00:00:00";
+	testWrapper.style.borderColor = "grey";
+}
+
+// Event listeners for keyboard input and the reset
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck);
+resetButton.addEventListener("click", reset, false);
+
+//testText[Math.random()*testTexts];
